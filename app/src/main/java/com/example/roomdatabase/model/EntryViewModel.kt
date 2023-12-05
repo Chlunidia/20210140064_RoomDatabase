ca26a272e855dc7a4d18e6b970c0a349f8d1191b
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-05 11:10:00 +0700

    Define StudentsDetails data class

---
+++ app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.roomdatabase.model
+
+class EntryViewModel {
+}
+
+data class StudentDetails(
+    val id: Int = 0,
+    val name: String = "",
+    val address: String = "",
+    val telp: String = "",
+)
