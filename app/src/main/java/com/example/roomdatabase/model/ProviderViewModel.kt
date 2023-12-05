cab58252087cc1839738a78d1ebcc27abff3d9f0
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-05 11:26:24 +0700

    Define viewModelFactory for HomeViewModel and EntryViewModel

--- app/src/main/java/com/example/roomdatabase/model/ProviderViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/model/ProviderViewModel.kt
@@ -6,5 +6,14 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.StudentsApp
 
+object ProviderViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(studentsApp().container.studentsRepository)
+        }
+        initializer { EntryViewModel(studentsApp().container.studentsRepository) }
+    }
+}
+
 fun CreationExtras.studentsApp(): StudentsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as StudentsApp)
