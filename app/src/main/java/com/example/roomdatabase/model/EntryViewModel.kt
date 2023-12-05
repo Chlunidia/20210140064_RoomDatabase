d5ee796a6f410b4f486d66fe7c41752a9e955bbe
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-05 11:10:24 +0700

    Define StudentsUIState data class

--- app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
@@ -3,6 +3,11 @@ package com.example.roomdatabase.model
 class EntryViewModel {
 }
 
+data class StudentsUIState(
+    val studentDetails: StudentDetails = StudentDetails(),
+    val isEntryValid: Boolean = false
+)
+
 data class StudentDetails(
     val id: Int = 0,
     val name: String = "",
