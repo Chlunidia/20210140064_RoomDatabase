b5f8b0f779a9df99e8ce00819e47e73c45903756
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-06 11:17:15 +0700

    Define EntryDestination as a NavigationDestination

---
+++ app/src/main/java/com/example/roomdatabase/ui/page/EntryPage.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase.ui.page
+
+import com.example.roomdatabase.R
+import com.example.roomdatabase.navigation.NavigationDestination
+
+object EntryDestination : NavigationDestination {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
