ef5c75ccb2d013a5fce8c7c835142aa9c9248e03
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-06 11:25:14 +0700

    Define coroutine scope and scroll behavior

--- app/src/main/java/com/example/roomdatabase/ui/page/EntryPage.kt
+++ app/src/main/java/com/example/roomdatabase/ui/page/EntryPage.kt
@@ -11,12 +11,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomdatabase.R
+import com.example.roomdatabase.model.EntryViewModel
+import com.example.roomdatabase.model.ProviderViewModel
 import com.example.roomdatabase.model.StudentDetails
 import com.example.roomdatabase.model.StudentsUIState
 import com.example.roomdatabase.navigation.NavigationDestination
@@ -24,6 +29,17 @@ import com.example.roomdatabase.navigation.NavigationDestination
 object EntryDestination : NavigationDestination {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StudentsEntryScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = ProviderViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
 
 @Composable
