feab27e86d5bd070cb59ac7a23d91c70d7cb1b24
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-06 11:40:24 +0700

    Define scroll behavior for the top app bar

--- app/src/main/java/com/example/roomdatabase/ui/page/HomePage.kt
+++ app/src/main/java/com/example/roomdatabase/ui/page/HomePage.kt
@@ -12,9 +12,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,13 +24,26 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomdatabase.R
 import com.example.roomdatabase.data.Students
+import com.example.roomdatabase.model.HomeViewModel
+import com.example.roomdatabase.model.ProviderViewModel
 import com.example.roomdatabase.navigation.NavigationDestination
 
 object HomeDestination : NavigationDestination {
     override val route = "home"
     override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = ProviderViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
 
 @Composable
