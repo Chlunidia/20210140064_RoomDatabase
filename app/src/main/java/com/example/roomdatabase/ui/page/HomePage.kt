0c0f348155634f9e1a8db7b196eace52d4b0406c
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-06 11:28:42 +0700

    Define NavigationDestination and set the route and the title resource for the home screen

---
+++ app/src/main/java/com/example/roomdatabase/ui/page/HomePage.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase.ui.page
+
+import com.example.roomdatabase.R
+import com.example.roomdatabase.navigation.NavigationDestination
+
+object HomeDestination : NavigationDestination {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
