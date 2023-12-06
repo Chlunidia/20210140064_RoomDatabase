430d700256ed8c091033cb0f6834697ff35c79d8
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-06 11:45:32 +0700

    Replace Greeting composable with StudentsApp in Surface and update GreetingPreview to use StudentsApp

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.roomdatabase.navigation.StudentsApp
 import com.example.roomdatabase.ui.theme.RoomDatabaseTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    StudentsApp()
                 }
             }
         }
@@ -41,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     RoomDatabaseTheme {
-        Greeting("Android")
+        StudentsApp()
     }
 }
